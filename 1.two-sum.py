5ab8522b84a1ddb9ba94cca9f15f00292890b472
beni1028 <beni1028@gmail.com>
2023-02-17 12:03:56 +0530

    Leet Code Day 1

---
+++ 1.two-sum.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums, target: int):
+        numToIndex = {}
+        for i in range(len(nums)):
+            if target - nums[i] in numToIndex:
+                return [numToIndex[target - nums[i]], i]
+            numToIndex[nums[i]] = i
+        return []
+            
+# @lc code=end
